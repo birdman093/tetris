f1a82ffc8af273085d83a671176137b3cb31b5c9
birdman093 <russfeathers@gmail.com>
2024-01-07 21:30:29 -0500

    Collisions, Pieces

--- tetris/src/app/page.tsx
+++ tetris/src/app/page.tsx
@@ -1,14 +1,22 @@
 "use client"
 import Link from "next/link";
-import { Dispatch, SetStateAction, useState } from "react";
-import { Piece } from "./piece";
+import { Dispatch, SetStateAction, useState, useEffect, useRef } from "react";
+import _ from 'lodash';
+import { Piece, Location } from "./piece";
 
 import styles from "./index.module.css";
-import { boolean } from "zod";
 
 const ROWS = 20;
 const COLS = 10;
-const GAMESTATES = ["PLAYING", "NONE", "PAUSED"];
+const NOGAME = "NONE";
+const PLAYING = "PLAYING";
+const PAUSED = "PAUSED";
+const GAMESTATES = [PLAYING, NOGAME, PAUSED];
+const BOUNDARY = "BOUNDARY";
+const BOTTOM = "BOTTOM";
+const PIECE = "PIECE";
+const NONE = "NONE"
+const COLLISIONSTATES = [BOUNDARY, BOTTOM, PIECE, NONE];
 
 function resetBoard() { 
   let matrix = [];
@@ -23,20 +31,170 @@ function resetBoard() {
 }
 
 function createPiece() {
-  //TODO:
-  return new Piece(0,0,0,"red");
+  return new Piece(ROWS/2,2,0,-1,"red");
 }
 
 export default function Home() {
 
-  const [game, setGame] = useState("NONE");
+  const [game, setGame] = useState(NOGAME);
   const [board, setBoard] = useState(resetBoard());
   const [piece, setPiece] = useState<Piece|null>(null);
   const [score, setScore] = useState(0);
   const [highscore, setHighScore] = useState(0);
-  
+  const [speed, setSpeed] = useState(1000);
+
+  // Handles stale state from functions defined in useEffect
+  const gameRef = useRef(game);
+    useEffect(() => {
+        gameRef.current = game;
+    }, [game]);
+  const pieceRef = useRef(piece);
+  useEffect(() => {
+      pieceRef.current = piece;
+  }, [piece]);
+
+  // game arrow handlers
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (gameRef.current === PLAYING && pieceRef.current != null){
+        switch (event.key) {
+          case 'ArrowUp':
+            movePiece(0,0,1);
+            console.log('Up key pressed');
+            break;
+          case 'ArrowDown':
+            movePiece(0,1,0);
+            console.log('Down key pressed');
+            break;
+          case 'ArrowLeft':
+            movePiece(-1,0,0);
+            console.log('Left key pressed');
+            break;
+          case 'ArrowRight':
+            movePiece(1,0,0);
+            console.log('Right key pressed');
+            break;
+          default:
+            // Handle other keys
+            break;
+          }
+      }
+    };
+
+    // Add event listener to the window object
+    window.addEventListener('keydown', handleKeyPress);
+
+    // Cleanup function to remove the event listener
+    return () => {
+        window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
+  const startPiece = (newPiece: Piece) => {
+    // clear current piece
+    if (piece != null){
+      clearInterval(piece.speed)
+    }
+
+    // set timer on new piece
+    newPiece.speed = setInterval(movePiece, speed, [0,1,0])
+    setPiece(newPiece);
+  }
+
+  const movePiece = 
+  (xOffset: number, yOffset: number, rotationOffset: number) => {
+    if (piece != null) {
+      // side boundary collision -- do not move piece
+      if (sideCollisionCheck(piece.getAllOffsetPoints(xOffset, yOffset)))
+      {
+        return;
+      }
+
+      // bottom boundary collision -- update board, create new piece
+      if (bottomCollisionCheck(piece.getAllOffsetPoints(xOffset, yOffset)))
+      {
+        updateBoard(piece.getAllPoints());
+        startPiece(createPiece());
+        return;
+      }
+
+      // piece collision -- update board, create new piece
+      if (pieceCollisonCheck(piece.getAllOffsetPoints(xOffset, yOffset))) {
+        updateBoard(piece.getAllPoints());
+        startPiece(createPiece());
+        return;
+      }
+
+      // no collision -- update piece
+     startPiece(new Piece(
+        piece.x() + xOffset, 
+        piece.y() + yOffset, 
+        piece.rotation + rotationOffset,
+        piece.speed, 
+        piece.color
+      ))
+    }
+    
+  };
+
+  const pieceCollisonCheck = (points: Location[]) => {
+    points.forEach(point => {
+      if (board && board.length > point.x() && point.x() >= 0){
+        const boardrow = board[point.x()]
+        if (boardrow && boardrow.length > point.y() && point.y() >= 0 
+        && boardrow[point.y()] == true){
+          return true;
+        }
+      }
+    });
+    return false;
+  }
+
+  const sideCollisionCheck = (points: Location[]): boolean => {
+    points.forEach(point => {
+      if (point.x() < 0 || point.x() >= COLS){
+        return true;
+      }
+    });
+    return false;
+  }
+
+  const bottomCollisionCheck = (points: Location[]): boolean => {
+    points.forEach(point => {
+      if (point.y() >= ROWS){
+        return true;
+      }
+    });
+    return false;
+  }
+
+  const updateBoard = (points: Location[]) => {
+    const newBoard = _.cloneDeep(board);
+    if (!newBoard){
+      console.log("ERROR: Update Board - Cloning")
+      return;
+    }
+    points.forEach(point => {
+      const x = point.x();
+      const y = point.y();
+      if (x >= 0 && x < COLS && y >= 0 && y < ROWS) {
+        const row = newBoard[x];
+        if (row && y < row.length) {
+            row[y] = true;
+        }
+      }
+    });
+
+    setBoard(newBoard);
+
+    // Clear Lines
+
+  }
+
+
+
   function handleNewGame() {
-    if (game != "NONE") {
+    if (game != NOGAME) {
       const userResponse = 
       confirm('Game is ongoing! Do you want to start a new game anyway?');
       if (!userResponse){ return;}
@@ -48,17 +206,39 @@ export default function Home() {
     setPiece(createPiece());
   }
 
+  const displayBoard = () => {
+
+    const grid = []
+    for (let i = 0; i < ROWS; i++) {
+      const row = [];
+      for (let j = 0; j < COLS; j++) {
+          row.push(
+              <div key={`${i}-${j}`} className={styles.gridSquare}></div>
+          );
+      }
+      grid.push(
+          <div key={i} className={styles.gridRow}>{row}</div>
+      );
+    }
+
+
+    return (
+      <div className={styles.board}>
+      </div>)
+  }
+
+  
+
   return (
     <main className={styles.main}>
       <div className = {styles.gamestats}>
         <button onClick={handleNewGame}>New Game</button>
-        <h6 className = {styles.scores}>High Score: {score}</h6>
-        <h3 className = {styles.scores}>Score: {highscore}</h3>
+        <h6 className = {styles.scores}>High Score : {score}</h6>
+        <h3 className = {styles.scores}>Score : {highscore}</h3>
+        <h3>Game State : {game}</h3>
       </div>
       <div className={styles.canvas}>
-          <div className={styles.board}>
-            {/* Board content goes here */}
-          </div>
+        {displayBoard()}
       </div>
     </main>
   );
--- tetris/src/app/piece.tsx
+++ tetris/src/app/piece.tsx
@@ -1,33 +1,73 @@
+export class Location {
+    X: number;
+    Y: number;
+    constructor(x: number, y: number){
+        this.X = x;
+        this.Y = y;
+    }
+
+    x():number {
+        return this.X;
+    }
+
+    y():number {
+        return this.Y;
+    }
+}
+
 export class Piece {
-    x: number;
-    y: number;
+    location: Location
     rotation: number;
     color: string;
+    speed: number;
 
-    constructor(x: number, y: number, rotation: number, color: string) {
-        this.x = x;
-        this.y = y;
+    constructor(x: number, y: number, rotation: number, speed: number, color: string) {
+        this.location = new Location(x, y);
         this.rotation = rotation;
+        this.speed = speed;
         this.color = color;
-    }
-
-    setPosition(x: number, y: number): void {
-        this.x = x;
-        this.y = y;
-    }
-
-    setRotation(rotation: number): void {
-        this.rotation = rotation;
     }
 
     setColor(color: string): void {
         this.color = color;
     }
 
+    x(): number {
+        return this.location.x();
+    }
+    
+    y(): number {
+        return this.location.y();
+    }
+
+    getAllPoints(): Location[]{
+        return [new Location(this.x(), this.y()),
+            new Location(this.x(), this.y()-1),
+            new Location(this.x(), this.y()+1),
+            new Location(this.x()+1, this.y()+1),
+            new Location(this.x()+1, this.y()),
+            new Location(this.x()+1, this.y()-1),
+            new Location(this.x()-1, this.y()+1),
+            new Location(this.x()-1, this.y()),
+            new Location(this.x()-1, this.y()-1)];
+    }
+
+    getAllOffsetPoints(x: number, y: number): Location[]{
+        return [new Location(this.x()+x, this.y()+y),
+            new Location(this.x()+x, this.y()-1+y),
+            new Location(this.x()+x, this.y()+1+y),
+            new Location(this.x()+1+x, this.y()+1+y),
+            new Location(this.x()+1+x, this.y()+y),
+            new Location(this.x()+1+x, this.y()-1+y),
+            new Location(this.x()-1+x, this.y()+1+y),
+            new Location(this.x()-1+x, this.y()+y),
+            new Location(this.x()-1+x, this.y()-1+y)];
+    }
+
     getState(): { x: number; y: number; rotation: number; color: string } {
         return {
-            x: this.x,
-            y: this.y,
+            x: this.x(),
+            y: this.y(),
             rotation: this.rotation,
             color: this.color
         };
